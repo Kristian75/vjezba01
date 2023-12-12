92a5332ce87dc368eb3a39118c880a85877e65e7
Kristian75 <152974653+Kristian75@users.noreply.github.com>
2023-12-12 12:20:40 +0100

    Add files via upload

---
+++ vjezba01.cs
@@ -0,0 +1,17 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		int a=100;
+		int b=3;
+		int x,y;
+		
+		x=a/b;
+		y=a%b;
+		
+		Console.WriteLine("Rezultat dijeljenja je: {0}", +x );
+		Console.WriteLine("Ostatak dijeljenja je: {0}", +y );
+}
+}
